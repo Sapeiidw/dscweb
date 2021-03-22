2618fd677f77bda21dbec6f08a0b961d90723ce7
Sapeiidw <sapeiidw@gmail.com>
2021-03-22 15:35:58 +0800

    Install Breeze, Ganti Route, Auth

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
+
+
+Route::view('/', 'welcome');
+Route::view('/dashboard', 'dashboard')->middleware(['auth'])->name('dashboard');
+require __DIR__.'/auth.php';

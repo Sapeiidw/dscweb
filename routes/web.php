9b561ddd2b7d0f3ff831d3082751ed846919217b
Sapeiidw <sapeiidw@gmail.com>
2021-03-22 18:44:54 +0800

    Create UserController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,4 +25,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 Route::view('/dashboard', 'dashboard')->middleware(['auth'])->name('dashboard');
+
+Route::resource('user', UserController::class)->shallow();
 require __DIR__.'/auth.php';
